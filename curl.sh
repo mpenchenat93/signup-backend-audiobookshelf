080293a7014940ce6e98b4e8e35104f7d2cfd3ac
Matthieu Penchenat <matthieu.penchenat@webinage.fr>
2024-12-05 23:07:43 +0100

    commit$

--- curl.sh
+++ curl.sh
@@ -1,3 +1,7 @@
 curl -X POST http://localhost:3000/api/signup \
 -H "Content-Type: application/json" \
 -d '{"email":"test@example.com","name":"John Doe","password":"secret"}'
+
+curl -X POST https://backend-topaz-tau.vercel.app/api/api/signup \
+-H "Content-Type: application/json" \
+-d '{"email":"test@example.com","name":"John Deer","password":"secret"}'
